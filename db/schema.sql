259a9e6114bf249afea6124991c98353759b260d
Pao Her <p.her007@hotmail.com>
2022-02-17 16:32:01 -0600

    create a votes table

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,7 @@
 -- table on the existence of a parties.id. In the same regard, the candidates table
 -- must be dropped before the parites table due to the foreigin key constraint that 
 -- requires the parties table to exist.
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
@@ -28,4 +29,14 @@ CREATE TABLE voters (
     last_name VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
