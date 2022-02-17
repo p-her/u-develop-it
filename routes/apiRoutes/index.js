3e0cef3534a6a9ba69315b9f241260750a50dffc
Pao Her <p.her007@hotmail.com>
2022-02-17 16:53:33 -0600

    create an api route to get vote totals

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const req = require('express/lib/request');
 const router = express.Router();
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 
 
