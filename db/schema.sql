71de3d6565ff66929ce602a636cafe812ae79095
Pao Her <p.her007@hotmail.com>
2022-02-17 01:03:57 -0600

    add a candidate route to change their party

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
+-- the order of table creation is vital due to the dependency of the candidates
+-- table on the existence of a parties.id. In the same regard, the candidates table
+-- must be dropped before the parites table due to the foreigin key constraint that 
+-- requires the parties table to exist.
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
 
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -10,5 +16,7 @@ CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL 
 );
--- server.js
+++ server.js
@@ -1,151 +1,223 @@
+const mysql = require("mysql2");
 const express = require('express');
-const mysql = require('mysql2');
 const inputCheck = require('./utlis/inputCheck');
-
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'fishB0n3#mysql',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
-);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-
-
-
-// err, which is the error response, if there are no errors
-// in the sql query, the err value is null
-
-// rows, which is the database query response
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
-
-
-
-// we assign the captured value populated in the req.params object
-// with the key id to params.
-app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
-
-    db.query(sql, params, (err, row) => {
-        if(err){
-            res.status(400).json({error: err.message});
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
+// Connect to database
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'fishB0n3#mysql',
+  database: 'election'
 });
 
-
-
-
-// ? denotes a placeholder, making this a prepared statement
-// a prepared statement can execute the same sql statements repeatedly using 
-// different values in place of the placeholder
-// affectedRows is 1 = meaning that only one row was affected by the DELETE command
-
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
-
-    db.query(sql, params, (err, result) => {
-        if(err){
-            res.statusMessage(400).json({ error: res.message});
-        }else if(!result.affectedRows){
-            // if there are no affectedRows as a result of the delete query, that means
-            // that there was no candidate by that id
-            res.json({
-                messsage: 'Candidate not found'
-            });
-        }else{
-            res.json({
-                message: 'deleted',
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
-
-// create a candidate
-// we use object destructuring to pull the body property out of the
-// request object, until now we've been passing the entire request object to
-// the routes in the req parameter.
-app.post('/api/candidate', ({body}, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if(errors){
-        res.status(400).json({error: errors});
-        return;
-    }
-
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-                VALUES (?, ?, ?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, (err, result) =>{
-        if(err){
-            res.status(400).json({error: err.message});
-            return ;
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
-    });
-});
-
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?, ?, ?, ?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//     if(err){
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
-
+// Get all candidates and their party affiliation
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-
-    db.query(sql, (err, rows) => {
-        if(err){
-            res.status(500).json({error: err.message});
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: rows
-        });
+  const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
+                
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
     });
+  });
 });
 
+// Get single candidate with party affiliation
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT candidates.*, parties.name 
+               AS party_name 
+               FROM candidates 
+               LEFT JOIN parties 
+               ON candidates.party_id = parties.id 
+               WHERE candidates.id = ?`;
+  const params = [req.params.id];
 
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
 
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  // Candidate is allowed not to be affiliated with a party
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
 
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected, party_id) VALUES (?,?,?,?)`;
+  const params = [
+    body.first_name,
+    body.last_name,
+    body.industry_connected,
+    body.party_id
+  ];
 
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body,
+      changes: result.affectedRows
+    });
+  });
+});
+
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+  // Candidate is allowed to not have party affiliation
+  const errors = inputCheck(req.body, 'party_id');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+    
+    
+    // ? denotes a placeholder, making this a prepared statement
+    // a prepared statement can execute the same sql statements repeatedly using
+    // different values in place of the placeholder
+    // affectedRows is 1 = meaning that only one row was affected by the DELETE command
+
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Get all parties
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Get single party
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Delete a party
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Party not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Not Found response for unmatched routes
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
-
-app.listen(PORT, () => {
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+  });
 });
