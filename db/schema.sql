ee353bbbf3c8befd74e912f26f9cb4c245b1afb6
Pao Her <p.her007@hotmail.com>
2022-02-16 23:34:27 -0600

    create and populate a parties table

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,10 @@
 
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
+
 
 CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
